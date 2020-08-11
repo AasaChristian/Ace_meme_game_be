1ebf8741e1f888cd0a30b0d869f64250477e12e9
Aasa-Christian <aasachristian@yahoo.com>
2020-08-11 19:45:02 -0400

    updated thread model

--- Models/thread-model.js
+++ Models/thread-model.js
@@ -3,16 +3,17 @@ const db = require("../db-config.js");
 module.exports ={
     findById,
     addto,
-
+    remove
 };
 
 function findById(){
-
     return db("thread")
 }
 
 function addto(message){
-    return db("thread").insert(message)
-    
+    return db("thread").insert(message).returning("*")
+}
 
+function remove(){
+    return db("thread").del()
 }
