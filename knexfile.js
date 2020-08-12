9f17820978b6e412fcd5f3ad72ac17031a2d22f7
Aasa-Christian <aasachristian@yahoo.com>
2020-08-12 16:18:40 -0400

    updated knexfile - production object

--- knexfile.js
+++ knexfile.js
@@ -33,18 +33,15 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'ace_meme',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./migrations"
     }
   }
 
